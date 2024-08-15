2c939be6df620d59abea022e3f24337180c3b652
Doug <doug.stalker@gmail.com>
2024-08-15 13:56:06 +1000

    It compiles and finds the PlayerInventory and open Container when a key is pushed.

---
+++ Install.bat
@@ -0,0 +1,1 @@
+copy "D:\TR\MoveToStack\bin\Debug\netstandard2.1\net.nep.bepinex.movetostack.dll"  "C:\Games\Steam\steamapps\common\Travellers Rest\Windows\BepInEx\plugins\"
---
+++ MoveToStack.cs
@@ -0,0 +1,90 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace MoveToStack
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static Harmony _harmony;
+        internal static ManualLogSource Log;
+
+        private static ConfigEntry<bool> _debugLogging;
+        private static ConfigEntry<KeyCode> _MoveToStackHotKey;
+
+
+        public Plugin()
+        {
+            // bind to config settings
+            _debugLogging = Config.Bind("Debug", "Debug Logging", false, "Logs additional information to console");
+            _MoveToStackHotKey = Config.Bind("General", "HotKey", KeyCode.F10, "Press to activate mod");
+        }
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Log = Logger;
+            _harmony = Harmony.CreateAndPatchAll(typeof(Plugin));
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void OnDestroy()
+        {
+            _harmony.UnpatchSelf();
+        }
+        public static void DebugLog(string message)
+        {
+            // Log a message to console only if debug is enabled in console
+            if (_debugLogging.Value)
+            {
+                Log.LogInfo(string.Format("MoveToStack: Debug: {0}", message));
+            }
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(_MoveToStackHotKey.Value))
+            {
+                PushToOpenContainer();
+            }
+        }
+
+
+
+        private static void PushToOpenContainer()
+        {
+            DebugLog(String.Format("PushToOpenContainer()"));
+            //just assume there is only one player 
+            PlayerInventory pi = PlayerInventory.GetPlayer(1);
+            if (pi is null)
+            {
+                DebugLog(String.Format("PushToOpenContainer(): failed to find player inventory"));
+                return;
+            }
+
+            ContainerUI targetContainer = null;
+            foreach (ContainerUI cui in UnityEngine.Object.FindObjectsOfType<ContainerUI>())
+            {
+                if (cui.IsOpen())
+                {
+                    targetContainer = cui;
+                    break;
+                }
+
+            }
+            if (targetContainer is null)
+            {
+                DebugLog(String.Format("PushToOpenContainer(): failed to find an open container"));
+                return;
+            }
+
+
+
+
+        }
+
+    }
+}
